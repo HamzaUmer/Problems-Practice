043b9f62fba5e9d5e16e410e218a1776505223f2
Mirza Hamza Umer <51873193+HamzaUmer@users.noreply.github.com>
2022-12-10 10:52:38 +0500

    Update vowels_and_consonant.js

--- vowels_and_consonant.js
+++ vowels_and_consonant.js
@@ -1,31 +1,3 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the vowelsAndConsonants function.
- * Print your output using 'console.log()'.
- */
 const vowel = ["a","e","i","o","u"];
 function vowelsAndConsonants(s) {
     let {vowels, consonants} = (s.split('') || []).reduce(
@@ -42,8 +14,8 @@ function vowelsAndConsonants(s) {
 }
 
 
-function main() {
-    const s = readLine();
-    
-    vowelsAndConsonants(s);
-}
+
+const s = "javascriptloops";
+
+vowelsAndConsonants(s);
+
