2d512f6d1f76824159334712a62a76eacf0f3285
Mirza Hamza Umer <51873193+HamzaUmer@users.noreply.github.com>
2022-12-10 11:13:30 +0500

    Create PositiveNumber_using_throw.js

---
+++ PositiveNumber_using_throw.js
@@ -0,0 +1,26 @@
+function isPositive(a) {
+    let result = "YES";
+    
+    if(a < 0) {
+        throw {message: "Negative Error"};
+    }
+    if(a===0) {
+        throw {message: "Zero Error"};
+    }
+    
+    return result
+}
+
+
+
+  const n = [1,2,3,4,5,6];
+
+  for (let i = 0; i < n; i++) {
+      const a = 3;
+
+      try {
+          console.log(isPositive(a));
+      } catch (e) {
+          console.log(e.message);
+      }
+  }
