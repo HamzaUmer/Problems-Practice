a6b31e58893bef191cfa74f2a468368267ff5798
Mirza Hamza Umer <51873193+HamzaUmer@users.noreply.github.com>
2022-12-10 11:36:28 +0500

    Update PositiveNumber_using_throw.js

--- PositiveNumber_using_throw.js
+++ PositiveNumber_using_throw.js
@@ -16,7 +16,7 @@ function isPositive(a) {
   const n = 3;
 
   for (let i = 0; i < n; i++) {
-      const a = [1,2,3,4};
+      const a = [1,2,3,4];
 
       try {
           console.log(isPositive(a));
