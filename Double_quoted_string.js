08671667cae489ada30ed9ee3c62ce826e51366d
Mirza Hamza Umer <51873193+HamzaUmer@users.noreply.github.com>
2022-12-19 12:28:46 +0500

    Create Double_quoted_string.js

---
+++ Double_quoted_string.js
@@ -0,0 +1,4 @@
+//This is Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+
+//Output would be I am a "double quoted" string inside "double quotes".
