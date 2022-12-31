1145f09ac48010d8a26048a5cba952a2951fa49d
Mirza Hamza Umer <51873193+HamzaUmer@users.noreply.github.com>
2022-12-31 14:42:39 +0500

    Create Recursion_Range_Number.js

---
+++ Recursion_Range_Number.js
@@ -0,0 +1,21 @@
+/*Question: We have defined a function named rangeOfNumbers with two parameters. 
+The function should return an array of integers which begins with a number represented 
+by the startNum parameter and ends with a number represented by the endNum parameter. 
+The starting number will always be less than or equal to the ending number. 
+Your function must use recursion by calling itself and not use loops of any kind. 
+It should also work for cases where both startNum and endNum are the same. 
+ */
+
+function rangeOfNumbers(startNum, endNum) {
+  if(endNum<startNum) {
+     return [];
+  }else {
+    const array = rangeOfNumbers(startNum, endNum-1);
+    array.unshift(endNum);
+    return array;
+  }
+};
+
+console.log(rangeOfNumbers(2,7));
+
+
