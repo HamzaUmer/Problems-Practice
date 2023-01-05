316491b2c2ea4940e2d293d6ada29ad56d3e0c72
Mirza Hamza Umer <51873193+HamzaUmer@users.noreply.github.com>
2023-01-05 12:30:09 +0500

    Create Concise_declaritive_function.js

---
+++ Concise_declaritive_function.js
@@ -0,0 +1,25 @@
+/* Write Concise Declarative Functions with ES6
+When defining functions within objects in ES5, we have to use the keyword function as follows:
+*/
+
+const bicycle = {
+  gear: 2,
+  setGear: function(newGear) {
+    this.gear = newGear;
+  }
+};
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+/* With ES6, you can remove the function keyword and colon altogether when defining functions in objects. Here's an example of this syntax: */
+
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
