be66845d2a16f94dbb6b23947e8a51340d38e7ea
Mirza Hamza Umer <51873193+HamzaUmer@users.noreply.github.com>
2023-01-26 15:46:02 +0500

    Create Regular_Expressions_7.js

---
+++ Regular_Expressions_7.js
@@ -0,0 +1,14 @@
+/**Match Anything with Wildcard Period
+The wildcard character . will match any one character. The wildcard is also called dot and period. 
+You can use the wildcard character just like any other character in the regex. 
+For example, if you wanted to match hug, huh, hut, and hum, you can use the regex /hu./ to match all four words.
+**/
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
+console.log(result)
+
+/** Output 
+true
+**/
