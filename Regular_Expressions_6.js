e122152bdbcfd1838b15e7be6f4646ce8ae18b7b
Mirza Hamza Umer <51873193+HamzaUmer@users.noreply.github.com>
2023-01-26 15:43:27 +0500

    Update Regular_Expressions_6.js

--- Regular_Expressions_6.js
+++ Regular_Expressions_6.js
@@ -3,3 +3,7 @@ let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /Twinkle/gi; 
 let result = twinkleStar.match(starRegex); 
 console.log(result)
+
+/** Output
+["Twinkle","twinkle"]
+**/
